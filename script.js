bc26192284ddab6504c674dd6c0227592a8cd225
ilhammtg <97074148+ilhammtg@users.noreply.github.com>
2024-05-04 22:25:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add a sweet birthday message on page load
+document.addEventListener("DOMContentLoaded", function() {
+    const message = "Happy Birthday, my love!";
+    document.title = message;
+    console.log(message);
+});
